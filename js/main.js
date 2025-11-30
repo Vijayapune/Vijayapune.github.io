7da7f4269ad9237ed7b12c16c7132965163ca7d0
Vijayapune <vijayavasant.pune@gmail.com>
2025-12-01 01:50:12 +0530

    first website upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+   
+    $(document).on('click', 'a[href^="#"]', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 114
+        }, 300);
+        
+    });
+
+    baguetteBox.run('.baguetteBoxOne, .baguetteBoxfloorplan');
+
+    $(".show-hide-button").on('click', function(){
+            $('.hide-specification').toggleClass('hide');
+            $(this).find('span').toggleClass('hide')
+    });
+      
+});
